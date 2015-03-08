a231deaf55e62e6fe962c8a5f4dad79b4f3b0541
allanjenn <allanjenn@hotmail.com>
2015-03-08 16:34:05 -0400

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+	before_action :require_user, except: [:help, :contact]
+
+	def signup
+	end
+
+	def contact
+	end
+
+	def help
+	end
+
+	def about
+	end
+
+
+
+end
